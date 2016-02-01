b59ea5897708e73e85e00d8201a846ed183eca73
cmis <cmis@cmis-hs11.(none)>
2016-02-01 13:34:35 +0700

    nearly completed

---
+++ Assignment1.py
@@ -0,0 +1,21 @@
+myName = "Boss"
+myAge = 14.2
+myHeight = 1.68
+Squarelength = 5
+rectanglelength = 10
+rectangleheight = 5
+Agemonths = myAge * 12
+print Agemonths
+LifeExpectancy = 68.5 - myAge
+print LifeExpectancy
+HeightinFeet = myHeight * 3.28084
+print HeightinFeet
+AverageHeight = 1.63
+HeightDifference = myHeight - AverageHeight
+print  HeightDifference
+SquareArea = Squarelength * 5
+print SquareArea
+SquareVolume = Squarelength ** 3 / 2.0
+print SquareVolume
+FractionofArea = SquareArea / 9.0000000000
+print FractionofArea
