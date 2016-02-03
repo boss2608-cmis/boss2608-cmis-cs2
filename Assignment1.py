e61fbd21a6defdc5ef04bfecc367ed47b50a2e2b
cmis <cmis@cmis-hs11.(none)>
2016-02-03 08:32:47 +0700

    completed first assignment

--- Assignment1.py
+++ Assignment1.py
@@ -1,21 +1,36 @@
 myName = "Boss"
+# This is my name
 myAge = 14.2
+# This is my age
 myHeight = 1.68
+# This is my height
 Squarelength = 5
+# This is the length of a square 
 rectanglelength = 10
+# This is the length of a rectangle
 rectangleheight = 5
+# This is the height of a rectangle 
 Agemonths = myAge * 12
-print Agemonths
+# This is my age in months
 LifeExpectancy = 68.5 - myAge
-print LifeExpectancy
+# This is about how long i have left to live
 HeightinFeet = myHeight * 3.28084
-print HeightinFeet
+# This is my height in feet
 AverageHeight = 1.63
+# This variable equals the average height
 HeightDifference = myHeight - AverageHeight
-print  HeightDifference
+# This calculates the difference between m height and the average height for people my age
 SquareArea = Squarelength * 5
-print SquareArea
+# This calculates the squares area
 SquareVolume = Squarelength ** 3 / 2.0
-print SquareVolume
+# This calulates the cubes volume
 FractionofArea = SquareArea / 9.0000000000
-print FractionofArea
+# This calculates 1/9 of the squares area
+print "Hello, my name is " + str(myName) + ". My Age is " + str(myAge) + ". I am " + str(myHeight) + " meters tall. My age in months is " + str(Agemonths) + ". I have approximately " + str(LifeExpectancy) + " years remaining in my life. " 
+#This is a message with 5 variables useing the add sign
+print "Hello, my name is", myName,". My Age is", myAge,". I am", myHeight," meters tall. My age in months is", Agemonths,". I have approximately", LifeExpectancy," years remaining in my life."
+# This is a message with 5 variables using commas
+Face = ";)"
+# This is a variable for a winking smiley face
+print Face * 10000
+# This prints out 10000 winking smiley faces
