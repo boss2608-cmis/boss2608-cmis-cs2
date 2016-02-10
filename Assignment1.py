0ccd556541474c3fce32c47a681f2630f1917444
cmis <cmis@cmis-hs11.(none)>
2016-02-10 10:55:44 +0700

    edited both

--- Assignment1.py
+++ Assignment1.py
@@ -1,30 +1,43 @@
 myName = "Boss"
+print myName
 # This is my name
 myAge = 14.2
+print myAge
 # This is my age
 myHeight = 1.68
+print myHeight
 # This is my height
 Squarelength = 5
+print Squarelength
 # This is the length of a square 
 rectanglelength = 10
+print rectanglelength
 # This is the length of a rectangle
 rectangleheight = 5
+print rectangleheight
 # This is the height of a rectangle 
 Agemonths = myAge * 12
+print Agemonths
 # This is my age in months
 LifeExpectancy = 68.5 - myAge
+print LifeExpectancy
 # This is about how long i have left to live
 HeightinFeet = myHeight * 3.28084
+print HeightinFeet
 # This is my height in feet
 AverageHeight = 1.63
 # This variable equals the average height
 HeightDifference = myHeight - AverageHeight
+print HeightDifference
 # This calculates the difference between m height and the average height for people my age
 SquareArea = Squarelength * 5
+print SquareArea
 # This calculates the squares area
 SquareVolume = Squarelength ** 3 / 2.0
+print SquareVolume
 # This calulates the cubes volume
 FractionofArea = SquareArea / 9.0000000000
+print FractionofArea
 # This calculates 1/9 of the squares area
 print "Hello, my name is " + str(myName) + ". My Age is " + str(myAge) + ". I am " + str(myHeight) + " meters tall. My age in months is " + str(Agemonths) + ". I have approximately " + str(LifeExpectancy) + " years remaining in my life. " 
 #This is a message with 5 variables useing the add sign
--- functions.py
+++ functions.py
@@ -1,20 +1,33 @@
+import math
 def add(a, b):
 	return a+b
 x=add(3, 4)
 print x
-#
+# this adds number a and b
 def sub(a, b):
 	return a-b
 y=sub(5, 3)
 print y
-#
+# this subtracts a and b
 def mul(a, b):
 	return a*b
 w=mul(4, 4)
 print w
-#
+# this multiplies a and b
 def div(a, b):
 	return a/b
 	return float
-z=div(2, 3)
+z=div(2.0, 3)
 print z
+# this divides a and b
+def hours(a, b, c):
+	return a/b/c
+H=hours(86400, 60, 60)
+print H
+# this turns a into hours
+def area(a,b):
+	return math.pi* a **b
+
+print area(5,2)
+
+
