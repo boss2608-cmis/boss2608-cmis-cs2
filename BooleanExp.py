e938ad083f3140f4e734a5e950b1ac8a79197786
cmis <cmis@cmis-hs11.(none)>
2016-03-01 10:55:29 +0700

    incomplete

---
+++ BooleanExp.py
@@ -0,0 +1,21 @@
+def OS():
+	print "Chrome OS"
+
+def Loading():
+	print"""
+
+	"""
+	print "Downloading..."
+	print """
+	
+	
+	"""
+
+Choosesystem = True
+SecondSystem = False
+
+if Choosesystem or SecondSystem:
+	OS()
+	Loading()
+
+print "Complete"
