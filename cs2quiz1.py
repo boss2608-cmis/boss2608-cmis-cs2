1db4c92179c401946b07f4338d1d93ccc145d1b3
cmis <cmis@cmis-hs11.(none)>
2016-03-04 08:39:22 +0700

    checking test, incomplete

--- cs2quiz1.py
+++ cs2quiz1.py
@@ -1,13 +1,13 @@
 #Part 1: Terminology (15 points)
 #1 1pt) What is the symbol "=" used for?
 #The symbol "=", or assignment operator, assigns things to whatever it is assigned to.
-
+# 0: inadequite amount of information
 #2 3pts) Write a technical definition for 'function'
 #A function is a code that spits something out after recieving information, like a vending machine.
-
+# 3: similar definition
 #3 1pt) What does the keyword "return" do?
 #return returns the value or expression from the function.
-
+#0.5: used the word return while defining the word return
 #4 5pts) We know 5 basic data types. Write the name for each one and provide two
 #   examples of each below
 #	1: String
@@ -20,15 +20,16 @@
 #		True, False
 #	5: tuple
 #		x = ("I", "Am", "Awesome")
+#4.5: only had one example for tuple
 #5 2pts) What is the difference between a "function definition" and a 
 #        "function call"?
-# A function definition defines the function, telling it wat to do, and calling a finction is like using the function
-
+# A function definition defines the function, telling it what to do, and calling a function is like using the function
+#1.5: used the word define in the definition of function definition
 #6 3pts) What are the 3 phases that every computer program has? What happens in each of them
 #	1: Recieve information
 #	2: Process information
 #	3: Returns information
-#
+#1.5: did not explain what they do
 #Part 2: Programming (25 points)
 #Write a program that asks the user for the areas of 3 circles.
 #It should then calculate the diameter of each and the sum of the diameters 
@@ -46,9 +47,9 @@
 import math
 def area(a, b, c):
 	pi = math.pi
-	D1 = (((sqrt*a)/pi)*2)
-	D2 = (((sqrt*b)/pi)*2)
-	D3 = (((sqrt*c)/pi)*2)
+	D1 = (((sqrt(a))/pi)*2)
+	D2 = (((sqrt(b))/pi)*2)
+	D3 = (((sqrt(c))/pi)*2)
 	Total = D1 + D2 + D3
 	return D1
 	return D2
