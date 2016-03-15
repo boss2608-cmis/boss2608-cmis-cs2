84bae7e70f11e3f99c757041adc9ee277fdd6b1b
cmis <cmis@cmis-hs10.(none)>
2016-03-15 13:36:31 +0700

    incomplete

--- oneguess.py
+++ oneguess.py
@@ -1,6 +1,7 @@
 import random
 
-	raw_input("What is the minimum number? ")
-	raw_input("What is the maximum number? ")
-	print "I'm thinking of a number from 
+minino = raw_input("What is the minimum number? ")
+maxino = raw_input("What is the maximum number? ")
+print "I'm thinking of a number from", minino, "to", maxino
+PlayerGuess = raw_input("What do you think it is? ")
 
