1d5e48ba6589c90e6c06cf4597a155c58e96d9e1
boss2608-cmis <boss2608@cmis.ac.th>
2016-03-15 19:24:15 +0700

    Update oneguess.py
    i think it's complete

--- oneguess.py
+++ oneguess.py
@@ -1,7 +1,34 @@
 import random
+def numberRange(number1,number2):
+    return random.randint(number1,number2)
 
-minino = raw_input("What is the minimum number? ")
-maxino = raw_input("What is the maximum number? ")
-print "I'm thinking of a number from", minino, "to", maxino
-PlayerGuess = raw_input("What do you think it is? ")
+def PlayerResult(result,PlayersGuess):
+    if result > PlayersGuess:
+        sub = abs(PlayersGuess - result)
+        return "That is over by {}".format(sub)
+    else:
+        add = abs(PlayersGuess + result)
+        return "That is under by {}".format(add)
+
+def output(result, PlayersGuess, wrong):
+    if result == PlayersGuess:
+        print """The target was {}
+Your guess was {}
+That's correct! You must be a psychic!""".format(result, PlayersGuess)
+    else:
+        print """The target was {}
+Your guess was {}
+{}""".format(result, PlayersGuess, wrong)
+
+
+def main():
+    number1 = int(raw_input("What is the minimum number?"))
+    number2 = int(raw_input("What is the maximum number?"))
+    print "Im thinking of a number from {} to {}".format(number1, number2)
+    PlayersGuess = int(raw_input("What do you think it is?:"))
+    result = int(numberRange(number1, number2))
+    wrong = PlayerResult(result, PlayersGuess)
+    return output(result, PlayersGuess, wrong)
+
+main()
 
