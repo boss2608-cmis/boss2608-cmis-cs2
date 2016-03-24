02d435b68f8bed1de7ce3d4a1b299fe5f4eb6425
cmis <cmis@cmis-hs05.(none)>
2016-03-24 08:35:41 +0700

    started

--- conditionals.py
+++ conditionals.py
@@ -1,1 +1,17 @@
-#
+#My conditionals program will test the player on whether or not he knows some of Shakespeare's most famous quotes.
+
+import random
+
+def game():
+	print """We will test how well you know some of Shakespeaer's most famous quotes 
+
+Level 1:"""
+	print "To be, or not to be: that is the _____"
+	answer = raw_input("What is the missing word: ")
+	print "To be, or not to be: that is the {}".format(answer)
+	if answer == Question:
+		print "Correct, Well Done!"
+	else answer != Question:
+		print "Wrong!"
+game()
+	
