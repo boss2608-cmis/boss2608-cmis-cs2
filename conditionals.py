64c0a0f07d1bfd79cb2d6e84725dce48911b3cf8
cmis <cmis@cmis-hs05.(none)>
2016-03-29 13:34:21 +0700

    incomplete

--- conditionals.py
+++ conditionals.py
@@ -1,4 +1,4 @@
-#This text based game will be a horror game. 
+#This text based game will be a horror game. This horror game is about a detective who has a succesful job as a detective and has solved many of the hardest cases, however he hasnt been able to solve his 
 
 import random
 
