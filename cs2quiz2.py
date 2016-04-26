8aef275c39e373a399e019e1a26ed9654f8fbe26
cmis <cmis@cmis-hs05.(none)>
2016-04-26 08:37:54 +0700

    finished marking test

--- cs2quiz2.py
+++ cs2quiz2.py
@@ -4,12 +4,12 @@
 #b) logical operators
 #c) boolean values
 #
-#2) What does 'return' do?
+#2 + 1) What does 'return' do?
 # Return spits out the input in a function
 #
 #
 #
-#3) What are 2 ways indentation is important in python code?
+#3 + 2) What are 2 ways indentation is important in python code?
 #a) In a function, anything that is indented after the funtion definition is part of the funtion
 #b) 
 #
@@ -20,23 +20,23 @@
 #
 #problem1_a) 36
 #problem1_b) 1.732 (sqrt of 3)
-#problem1_c) 0
-#problem1_d) 5
+#problem1_c +1) 0
+#problem1_d +1) -5
 #
-#problem2_a) True
-#problem2_b) False
-#problem2_c) Flase
+#problem2_a +1) True
+#problem2_b +1) False
+#problem2_c +1) Flase
 #problem2_d) False
 #
-#problem3_a) 0.3
-#problem3_b) 0.5
-#problem3_c) 0.5
-#problem3_d) 0.5
+#problem3_a +1) 0.3
+#problem3_b +1) 0.5
+#problem3_c +1) 0.5
+#problem3_d +1) 0.5
 #
-#problem4_a) 7
-#problem4_b) 5
-#problem4_c) 0.125
-#problem4_d) 5
+#problem4_a +1) 7
+#problem4_b +1) 5
+#problem4_c +1) 0.125
+#problem4_d +1) 5
 #
 
 #PART 3: Programming
@@ -46,10 +46,11 @@
 #If they don't, it should print a message telling them they didn't follow 
 #the directions.
 #Be sure to use the program structure you've learned (main function, processing function, output function)
-
+#Q23+1
 def Comparison(A, B, C):
 	print """Type in 3 different numbers (decimals are OK!)"""
-
+#Q25+1
+#Q26+1
 def output():
      if int(A) > int(B) and int(A) > int(C):
         print "The largest numbe was " + int(A)
@@ -60,7 +61,8 @@ def output():
 	else:
 		print """you didn't follow directions"""
 
-
+#Q30+1
+#Q31+1
 def main():
 	print """Type in 3 different numbers (decimals are OK!)"""
     A = int(raw_input("A "))
