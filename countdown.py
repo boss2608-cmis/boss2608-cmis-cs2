3c0512af7577f3ebc08b29891b29237a1667d030
cmis <cmis@cmis-hs05.(none)>
2016-04-27 10:08:54 +0700

    incomplete

--- countdown.py
+++ countdown.py
@@ -1,18 +1,30 @@
-def countdown(start, stop):
-	if start < stop:
-		print start
-		print"done"
-	else:
-		print start
-		countdown(start-1, stop)
+#def countdown(start, stop):
+#	if start < stop:
+#		print start
+#		print"done"
+#	else:
+#		print start
+#		countdown(start-1, stop)
 
-def main():
-	start = int(raw_input("start: "))
-	stop = int(raw_input("stop: "))
-	countdown(start, stop)
-main()
+#def main():
+#	start = int(raw_input("start: "))
+#	stop = int(raw_input("stop: "))
+#	countdown(start, stop)
+#main()
 
 def adder():
-	no. = float(raw_input("next number: "))
-	if n0. == float(" ")
- 
+
+	n0 = float(raw_input("next number: "))
+	if n0 != "":
+		running_total(total)
+	else:
+		adder()
+adder()
+
+def running_total(total):
+	total = n0
+	total + n0
+	print total
+
+
+
