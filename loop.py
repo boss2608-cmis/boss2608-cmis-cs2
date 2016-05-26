72689381574316d3130fd2ab9b505066566f1aae
cmis <cmis@cmis-hs08.(none)>
2016-05-26 08:42:58 +0700

    incomplete

--- loop.py
+++ loop.py
@@ -2,12 +2,41 @@ def countdown(x):
 	while x >= 0:
 		print x
 		x -= 1
-countdown(7)
+#countdown(7)
 
 def countup(x):
 	while x <= 10:
 		print x
 		x += 1
-countup(0)
+#countup(0)
 
+def count(x):
+	while x < 0:
+		print x
+		x += 1
+	while x > 0:
+		print x
+		x -= 1
+#count(-5)
 
+def countfrom2(x, y):
+	while x < y:
+		print x
+		x += 1
+	while x > y:
+		print x
+		x -= 1
+	while x == y:
+		print y
+		x -= 1
+#countfrom2(5, 0)
+
+def addodds(n):
+	n = float(n)
+	if n % 2 == 0:
+		while n < 0:
+			return sum
+	elif n % 2 == 0:
+		while n > 0:
+			return sum
+addodds(21)
