be31ca551d7b28492227e0f4c12d35a0d106b9a8
cmis <cmis@cmis-hs05.(none)>
2016-05-27 10:56:43 +0700

    incomplete

--- loop.py
+++ loop.py
@@ -32,11 +32,39 @@ def countfrom2(x, y):
 #countfrom2(5, 0)
 
 def addodds(n):
-	n = float(n)
-	if n % 2 == 0:
-		while n < 0:
-			return sum
-	elif n % 2 == 0:
+	sum = 0
+	if n > 0:
+		print "its positive"
 		while n > 0:
-			return sum
-addodds(21)
+			if n % 2 == 1:
+				sum += n
+			n -= 1
+	elif n < 0:
+		print "its negative"
+		while n < 0:
+			if n % 2 == 1:
+				sum += n
+			n += 1
+	print sum
+#addodds(20)
+
+def grid(w, h):
+	a = "."*w
+	while h != 0:
+		print a
+		h = 1
+#grid(10, 10)
+
+def grid2(w, h):
+	out = ""
+	x = 0
+	while x < w:
+		out += "."
+		x += 1
+	while x < h:
+		out += "."
+		x += 1
+	print x
+grid2(10, 10)
+	 
+
